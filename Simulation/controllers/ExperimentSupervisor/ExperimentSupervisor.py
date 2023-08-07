530f4fd7fe861a0f3746597a1d18d1f196a940d6
Chance Hamilton <chamilton4@usf.edu>
2023-08-07 13:32:28 -0400

    Fixed issue with ROSbot_Lidar.proto

--- Simulation/controllers/ExperimentSupervisor/ExperimentSupervisor.py
+++ Simulation/controllers/ExperimentSupervisor/ExperimentSupervisor.py
@@ -1,9 +1,12 @@
+import os
+os.chdir("../..")
+
 """ExperimentSupervisor controller."""
 from BiologyTools.PlaceCellLibrary import PlaceCellNetwork
 from Simulation.libraries.RosBot import RosBot
 
 
-maze_file = 'worlds/mazes/experiment2/WM00.xml'
+maze_file = 'worlds/mazes/experiment2/WM20.xml'
 
 # create the robot/supervisor instance.
 robot = RosBot()
@@ -17,18 +20,15 @@ robot.move_to_random_start()
 # Creates Place Cell Network
 experiment_pc_network = PlaceCellNetwork()
 
-for i in range(5):
-    robot.preform_random_action()
+for i in range(8):
+    robot.preform_action(i)
     robot_x, robot_y, robot_theta = robot.get_robot_pose()
     if experiment_pc_network.get_num_active_pc(robot_x,robot_y) == 0:
         experiment_pc_network.add_pc_to_network(robot_x,robot_y,)
     experiment_pc_network.calculate_total_pc_activation(robot_x, robot_y)
-    experiment_pc_network.print_pc_activations()
+    # experiment_pc_network.print_pc_activations()
     robot.update_pc_display()
 
-#
-# robot.move_to_xy(1.0,1.0)
-
-# robot.experiment_supervisor.simulationReset()
+robot.experiment_supervisor.simulationReset()
 
 
